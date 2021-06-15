0035fbaf571b9d6aeb3ea0e913c54b0d0bcd078f
Ademir R. da Silva <ademiritumbiara@outlook.com>
2021-06-15 08:59:54 -0300

    Init EmployeeRole domain config

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,15 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole
+    {
+        public string Id { get; init; }
+        public string RoleName { get; init; }
+
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+    }
+}
